72c0734b8f09ec4b757c43662f4235d8239c35c0
Aman Kumar <amankumar.m99@gmail.com>
2025-09-09 21:24:51 +0530

    init code from ide

---
+++ src/main/java/com/spanprints/configserver/SpanPrintsConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.spanprints.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpanPrintsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpanPrintsConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spanprints/configserver/SpanPrintsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spanprints.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpanPrintsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
